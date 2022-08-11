f874c11684abff6d0332d59b9b8f601f6430e23e
Shuhui Liu <shuhliu@linkedin.com>
2022-08-11 09:42:27 -0700

    Change github workflow to publish to jfrog instead of bintray

---
+++ scripts/publishToJfrog.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+result=${PWD##*/}
+if [[ "$result" = "scripts" ]]
+then
+    echo "script must be run from root project folder, not $PWD"
+    exit 1
+else
+    echo "we are in $PWD and tag is $TRAVIS_TAG"
+
+    if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]
+    then
+        echo "tag $TRAVIS_TAG looks like a semver so proceeding with jfrog publish"
+        git status
+        git describe --tags
+        ./gradlew publishAllPublicationsToLinkedInJfrogRepository
+    else
+        echo "tag $TRAVIS_TAG is NOT a valid semantic version (x.y.z) so not publishing to jfrog"
+    fi
+fi
