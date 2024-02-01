da770ae69afbd66ba63cc2c0b74dfa43080388ff
Gaojie Liu <goliu@linkedin.com>
2024-02-01 10:12:19 -0800

    Updated javadoc

--- src/test/java/io/tehuti/utils/RedundantLogFilterTest.java
+++ src/test/java/io/tehuti/utils/RedundantLogFilterTest.java
@@ -3,6 +3,11 @@ package io.tehuti.utils;
 
 public class RedundantLogFilterTest {
 
+  /**
+   * Junit test shutdown will force kill all the live threads (non-daemon), so that here we use a cmd application
+   * to validate whether {@link RedundantLogFilter} would stuck or not when {@link RedundantLogFilter#shutdown()} is
+   * not explicitly invoked.
+   */
   public static void main(String[] args) throws InterruptedException {
     RedundantLogFilter logFilter = new RedundantLogFilter(10, 1000);
     logFilter.isRedundantLog("test");
